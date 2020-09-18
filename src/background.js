340d1b9ca6a8b870fdfc2fbd01981fa1a5f86445
Phuoc Nguyen <me@phuoc.ng>
2020-09-18 14:49:11 +0700

    Copy SVG's HTML to clipboard

--- src/background.js
+++ src/background.js
@@ -1,11 +1,16 @@
 ((ext) => {
     const copyHtmlMenu = ext.contextMenus.create({
         title: 'Copy HTML code',
-        contexts: ['all']
+        contexts: ['all'],
+        onclick: (info, tab) => {
+            ext.tabs.sendMessage(tab.id, {
+                action: 'ACTION_COPY',
+            });
+        },
     });
 
     const downloadMenu = ext.contextMenus.create({
         title: 'Download',
-        contexts: ['all']
+        contexts: ['all'],
     });
 })(chrome || browser);
